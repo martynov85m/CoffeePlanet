265052e5c9bd281c2bbef89bb68819381e61aacb
Anton Martynov <marantig7@gmail.com>
2020-01-26 23:57:27 +0200

    11 урок. Сделал перекючение вкладок

--- src/js/index.js
+++ src/js/index.js
@@ -10,4 +10,38 @@ window.addEventListener('load', function(){
 		navigation.classList.remove('isOpened');
 		mobileBtn.classList.remove('closer');
 	});
+
+	var firstTab = document.querySelector('.catalog__tab > li:first-child');
+	var secondTab = document.querySelector('.catalog__tab > li:nth-child(2)');
+	var thirdTab = document.querySelector('.catalog__tab > li:last-child');
+	var firstContent = document.querySelector('.catalog__content > li:first-child');
+	var secondContent = document.querySelector('.catalog__content > li:nth-child(2)');
+	var thirdContent = document.querySelector('.catalog__content > li:last-child');
+
+	firstTab.addEventListener('click', function(){
+		firstTab.classList.add('active');
+		secondTab.classList.remove('active');
+		thirdTab.classList.remove('active');
+		firstContent.classList.remove('d-n');
+		secondContent.classList.add('d-n');
+		thirdContent.classList.add('d-n');
+	});
+
+	secondTab.addEventListener('click', function(){
+		firstTab.classList.remove('active');
+		secondTab.classList.add('active');
+		thirdTab.classList.remove('active');
+		firstContent.classList.add('d-n');
+		secondContent.classList.remove('d-n');
+		thirdContent.classList.add('d-n');
+	});
+
+	thirdTab.addEventListener('click', function(){
+		firstTab.classList.remove('active');
+		secondTab.classList.remove('active');
+		thirdTab.classList.add('active');
+		firstContent.classList.add('d-n');
+		secondContent.classList.add('d-n');
+		thirdContent.classList.remove('d-n');
+	});
 });
